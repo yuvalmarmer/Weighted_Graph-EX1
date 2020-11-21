5311663771de313a82ae2aaf4e6a6f7a1eb5f3a7
יובל מרמר <yuvalmarmer97@gmail.com>
2020-11-21 15:15:07 +0200

    Fixing tests rep

--- tests/WGraph_AlgoTest.java
+++ tests/WGraph_AlgoTest.java
@@ -1,99 +1,131 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class WGraph_AlgoTest {
-
+public class WGraph_AlgoTest {
     @Test
-    void isConnected() {
-        weighted_graph g0 = WGraph_DSTest.graph_creator(0,0,1);
-        weighted_graph_algorithms ag0 = new WGraph_Algo();
-        ag0.init(g0);
-        assertTrue(ag0.isConnected());
-  
-        g0 = WGraph_DSTest.graph_creator(1,0,1);
-        ag0 = new WGraph_Algo();
-        ag0.init(g0);
-        assertTrue(ag0.isConnected());
+    @DisplayName("DeepCopy testing")
+    public void DeepCopyTest(){
+        WGraph_DS graph = new WGraph_DS();
+        //Adding 6 nodes into the graph and 2 edges
+        graph.addNode(1);
+        graph.addNode(2);
+        graph.addNode(3);
+        graph.addNode(4);
+        graph.addNode(5);
+        graph.addNode(6);
+        graph.connect(1,2,1);
+        graph.connect(1,3,2);
+        graph.connect(1,4,3);
+        weighted_graph_algorithms graph2 = new WGraph_Algo();
+        graph2.init(graph);
+        weighted_graph NewGraph = graph2.copy();
+        Assertions.assertEquals(NewGraph.nodeSize(),graph.nodeSize());
+        Assertions.assertEquals(NewGraph.getEdge(1,4), graph.getEdge(4,1));
 
-         g0 = WGraph_DSTest.graph_creator(2,0,1);
-        ag0 = new WGraph_Algo();
-        ag0.init(g0);
-        assertFalse(ag0.isConnected());
-        
-         g0 = WGraph_DSTest.graph_creator(2,1,1);
-        ag0 = new WGraph_Algo();
-        ag0.init(g0);
-        assertTrue(ag0.isConnected());
+    }
+    @Test
+    @DisplayName("isConnected algo test")
+    public void isConnectedTest(){
+        WGraph_DS graph = new WGraph_DS();
+        graph.addNode(1);
+        graph.addNode(2);
+        graph.addNode(3);
+        graph.addNode(4);
+        graph.connect(1,2,1);
+        graph.connect(2,3,1);
+        graph.connect(3,4,1);
+        weighted_graph_algorithms graph2 = new WGraph_Algo();
+        graph2.init(graph);
+        Assertions.assertTrue(graph2.isConnected());
 
-        g0 = WGraph_DSTest.graph_creator(10,30,1);
-        ag0.init(g0);
-        boolean b = ag0.isConnected();
-        assertTrue(b);
     }
 
     @Test
-    void shortestPathDist() {
-        weighted_graph g0 = small_graph();
-        weighted_graph_algorithms ag0 = new WGraph_Algo();
-        ag0.init(g0);
-        //assertTrue(ag0.isConnected());
-        double d = ag0.shortestPathDist(0,10);
-        assertEquals(d, 5.1);
+    @DisplayName("Checking the ShortestPath dist algo")
+    public void shortestPathDist(){
+        WGraph_DS graph = new WGraph_DS();
+        graph.addNode(0);
+        graph.addNode(1);
+        graph.addNode(2);
+        graph.addNode(3);
+        graph.addNode(4);
+        graph.connect(0,1,2);
+        graph.connect(1,2,1);
+        graph.connect(1,4,6);
+        graph.connect(2,3,2);
+        graph.connect(3,4,1);
+        weighted_graph_algorithms graph2 = new WGraph_Algo();
+        graph2.init(graph);
+        Assertions.assertEquals(graph2.shortestPathDist(0,4),6);
     }
 
     @Test
-    void shortestPath() {
-        weighted_graph g0 = small_graph();
-        weighted_graph_algorithms ag0 = new WGraph_Algo();
-        ag0.init(g0);
-        List<node_info> sp = ag0.shortestPath(0,10);
-        //double[] checkTag = {0.0, 1.0, 2.0, 3.1, 5.1};
-        int[] checkKey = {0, 1, 5, 7, 10};
-        int i = 0;
-        for(node_info n: sp) {
-        	//assertEquals(n.getTag(), checkTag[i]);
-        	assertEquals(n.getKey(), checkKey[i]);
-        	i++;
+    @DisplayName("Checking the shortestPath")
+    public void shortestPath(){
+        WGraph_DS graph = new WGraph_DS();
+        graph.addNode(0);
+        graph.addNode(1);
+        graph.addNode(2);
+        graph.addNode(3);
+        graph.addNode(4);
+        graph.connect(0,1,2);
+        graph.connect(1,2,1);
+        graph.connect(1,4,6);
+        graph.connect(2,3,2);
+        graph.connect(3,4,1);
+        weighted_graph_algorithms graph2 = new WGraph_Algo();
+        graph2.init(graph);
+        //5 Nodes in the path from 0-4
+        Assert.assertEquals(graph2.shortestPath(0,4).size(), 5);
+    }
+
+    @Test
+    @DisplayName("Check Save and Load functions")
+    public void saveLoad(){
+        WGraph_DS graph = new WGraph_DS();
+        for(int i=0;i<1000;++i){
+            graph.addNode(i);
         }
-    }
-    
-    @Test
-    void save_load() {
-        weighted_graph g0 = WGraph_DSTest.graph_creator(10,30,1);
-        weighted_graph_algorithms ag0 = new WGraph_Algo();
-        ag0.init(g0);
-        String str = "g0.json";
-        ag0.save(str);
-        weighted_graph g1 = WGraph_DSTest.graph_creator(10,30,1);
-        ag0.load(str);
-        assertEquals(g0,g1);
-        g0.removeNode(0);
-        assertNotEquals(g0,g1);
-    }
-    @Test
-    private weighted_graph small_graph() {
-        weighted_graph g0 = WGraph_DSTest.graph_creator(11,0,1);
-        g0.connect(0,1,1);
-        g0.connect(0,2,2);
-        g0.connect(0,3,3);
+        Random r = new Random(1);
+        for(int i=0;i<100;i++){
+            graph.connect(i, i+100,r.nextDouble()*1000);
+        }
 
-        g0.connect(1,4,17);
-        g0.connect(1,5,1);
-        g0.connect(2,4,1);
-        g0.connect(3, 5,10);
-        g0.connect(3,6,100);
-        g0.connect(5,7,1.1);
-        g0.connect(6,7,10);
-        g0.connect(7,10,2);
-        g0.connect(6,8,30);
-        g0.connect(8,10,10);
-        g0.connect(4,10,30);
-        g0.connect(3,9,10);
-        g0.connect(8,10,10);
+        WGraph_Algo algo = new WGraph_Algo();
+        algo.init(graph);
+        algo.save("TestSave.json");
+        WGraph_Algo algo2 = new WGraph_Algo();
+        algo2.load("TestSave.json");
+        Assert.assertEquals(algo2.getGraph().edgeSize(), graph.edgeSize());
+        Assert.assertEquals(algo2.getGraph().nodeSize(), graph.nodeSize());
+    }
 
-        return g0;
+
+    @Test
+    @DisplayName("Huge graph test")
+    public void bigGraph(){
+        WGraph_DS graph = new WGraph_DS();
+        for(int i=0;i<1000000;++i){
+            graph.addNode(i);
+        }
+        Random r = new Random(1);
+        for(int i=0;i<100000;i++){
+            graph.connect(i, i+1,r.nextDouble()*1000);
+        }
+        WGraph_Algo algo = new WGraph_Algo();
+        algo.init(graph);
+        algo.save("TestSave.json");
+        WGraph_Algo algo2 = new WGraph_Algo();
+        algo2.load("TestSave.json");
+        Assert.assertEquals(algo2.getGraph().edgeSize(), graph.edgeSize());
+        Assert.assertEquals(algo2.getGraph().nodeSize(), graph.nodeSize());
     }
 }
--- tests/WGraph_DSTest.java
+++ tests/WGraph_DSTest.java
@@ -1,188 +1,131 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-
-class WGraph_DSTest {
-    private static Random _rnd = null;
+public class WGraph_DSTest {
 
     @Test
-    void nodeSize() {
-        weighted_graph g = new WGraph_DS();
-        g.addNode(0);
-        g.addNode(1);
-        g.addNode(1);
-
-        g.removeNode(2);
-        g.removeNode(1);
-        g.removeNode(1);
-        int s = g.nodeSize();
-        assertEquals(1,s);
-
-    }
-
-    @Test
-    void edgeSize() {
-        weighted_graph g = new WGraph_DS();
-        g.addNode(0);
-        g.addNode(1);
-        g.addNode(2);
-        g.addNode(3);
-        g.connect(0,1,1);
-        g.connect(0,2,2);
-        g.connect(0,3,3);
-        g.connect(0,1,1);
-        int e_size =  g.edgeSize();
-        assertEquals(3, e_size);
-        double w03 = g.getEdge(0,3);
-        double w30 = g.getEdge(3,0);
-        assertEquals(w03, w30);
-        assertEquals(w03, 3);
-    }
-
-    @Test
-    void getV() {
-        weighted_graph g = new WGraph_DS();
-        g.addNode(0);
-        g.addNode(1);
-        g.addNode(2);
-        g.addNode(3);
-        g.connect(0,1,1);
-        g.connect(0,2,2);
-        g.connect(0,3,3);
-        g.connect(0,1,1);
-        Collection<node_info> v = g.getV();
-        Iterator<node_info> iter = v.iterator();
-        while (iter.hasNext()) {
-            node_info n = iter.next();
-            assertNotNull(n);
+    @DisplayName("Create weighted graph with 10 nodes and 0 edges")
+    public void creatGraph() {
+        WGraph_DS graph = new WGraph_DS();
+        for(int i=0;i<10;++i){
+            graph.addNode(i);
         }
+        Assert.assertEquals(graph.nodeSize(), 10);
+        Assert.assertEquals(graph.edgeSize(), 0);
     }
 
+    //Create weighted graph with 10 nodes and 10 edges with Random edges
     @Test
-    void hasEdge() {
-        int v = 10, e = v*(v-1)/2;
-        weighted_graph g = graph_creator(v,e,1);
-        for(int i=0;i<v;i++) {
-            for(int j=i+1;j<v;j++) {
-                boolean b = g.hasEdge(i,j);
-                assertTrue(b);
-                assertTrue(g.hasEdge(j,i));
-            }
+    @DisplayName("Graph with edges")
+    public void graphWithEdges() {
+        WGraph_DS graph = new WGraph_DS();
+        for(int i=0;i<10;++i){
+            graph.addNode(i);
         }
-    }
+        Random rand = new Random(1);
 
-    @Test
-    void connect() {
-        weighted_graph g = new WGraph_DS();
-        g.addNode(0);
-        g.addNode(1);
-        g.addNode(2);
-        g.addNode(3);
-        g.connect(0,1,1);
-        g.connect(0,2,2);
-        g.connect(0,3,3);
-        g.removeEdge(0,1);
-        assertFalse(g.hasEdge(1,0));
-        g.removeEdge(2,1);
-        g.connect(0,1,1);
-        double w = g.getEdge(1,0);
-        assertEquals(w,1);
-    }
-
-
-    @Test
-    void removeNode() {
-        weighted_graph g = new WGraph_DS();
-        g.addNode(0);
-        g.addNode(1);
-        g.addNode(2);
-        g.addNode(3);
-        g.connect(0,1,1);
-        g.connect(0,2,2);
-        g.connect(0,3,3);
-        g.removeNode(4);
-        g.removeNode(0);
-        assertFalse(g.hasEdge(1,0));
-        int e = g.edgeSize();
-        assertEquals(0,e);
-        assertEquals(3,g.nodeSize());
-    }
-
-    @Test
-    void removeEdge() {
-        weighted_graph g = new WGraph_DS();
-        g.addNode(0);
-        g.addNode(1);
-        g.addNode(2);
-        g.addNode(3);
-        g.connect(0,1,1);
-        g.connect(0,2,2);
-        g.connect(0,3,3);
-        g.removeEdge(0,3);
-        double w = g.getEdge(0,3);
-        assertEquals(w,-1);
-    }
-
-
-    ///////////////////////////////////
-    /**
-     * Generate a random graph with v_size nodes and e_size edges
-     * @param v_size
-     * @param e_size
-     * @param seed
-     * @return
-     */
-    public static weighted_graph graph_creator(int v_size, int e_size, int seed) {
-        weighted_graph g = new WGraph_DS();
-        _rnd = new Random(seed);
-        for(int i=0;i<v_size;i++) {
-            g.addNode(i);
+        for (int i = 0; i < 9; i++) {
+            double w = rand.nextDouble()*10;
+            System.out.println("Connect " + i + " with " + (i+1) + ", weight " +  w);
+            graph.connect(i,i+1,w);
         }
-        // Iterator<node_data> itr = V.iterator(); // Iterator is a more elegant and generic way, but KIS is more important
-        int[] nodes = nodes(g);
-        while(g.edgeSize() < e_size) {
-            int a = nextRnd(0,v_size);
-            int b = nextRnd(0,v_size);
-            int i = nodes[a];
-            int j = nodes[b];
-            double w = _rnd.nextDouble();
-            g.connect(i,j, w);
+        double w = rand.nextDouble()*10;
+        System.out.println("Connect 9 with 0, weight " +  w);
+        graph.connect(9,0,w);
+
+        Assert.assertEquals(graph.nodeSize(), 10);
+        Assert.assertEquals(graph.edgeSize(), 10);
+    }
+
+    //Create graph with 10 vertices and 10 edges and remove 3 of them
+    @Test
+    @DisplayName("Remove edges")
+    public void removeEdge() {
+        WGraph_DS graph = new WGraph_DS();
+        for(int i=0;i<10;++i){
+            graph.addNode(i);
         }
-        return g;
+        Random rand = new Random(1);
+
+        for (int i = 0; i < 9; i++) {
+            double w = rand.nextDouble()*10;
+            System.out.println("Connect " + i + " with " + (i+1) + ", weight " +  w);
+            graph.connect(i,i+1,w);
+        }
+        double w = rand.nextDouble()*10;
+        System.out.println("Connect 9 with 0, weight " +  w);
+        graph.connect(9,0,w);
+
+        graph.removeEdge(1,2);
+        graph.removeEdge(1,2);
+        graph.removeEdge(2,3);
+        graph.removeEdge(5,6);
+        Assert.assertEquals(graph.nodeSize(), 10);
+        Assert.assertEquals(graph.edgeSize(), 7);
     }
-    private static int nextRnd(int min, int max) {
-        double v = nextRnd(0.0+min, (double)max);
-        int ans = (int)v;
-        return ans;
+
+    //Create graph with 10 vertices with 5 edges to node '1' and delete it.
+    @Test
+    @DisplayName("Remove nodes")
+    public void removeNode() {
+        WGraph_DS graph = new WGraph_DS();
+        for(int i=0;i<10;++i){
+            graph.addNode(i);
+        }
+        Random rand = new Random(1);
+
+        for (int i = 0; i < 5; i++) {
+            double w = rand.nextDouble()*10;
+            System.out.println("Connect 0 with " + (i+1) + ", weight " +  w);
+            graph.connect(0,i+1,w);
+        }
+        node_info node = graph.removeNode(0);
+        graph.removeNode(0);
+        Assert.assertEquals(graph.nodeSize(), 9);
+        Assert.assertEquals(graph.edgeSize(), 0);
+        Assert.assertEquals(node.getKey(), 0);
     }
-    private static double nextRnd(double min, double max) {
-        double d = _rnd.nextDouble();
-        double dx = max-min;
-        double ans = d*dx+min;
-        return ans;
+
+    @Test
+    @DisplayName("Edge sized check")
+    public void edgeSize(){
+        WGraph_DS graph = new WGraph_DS();
+        for(int i=0;i<100;++i){
+            graph.addNode(i);
+        }
+        Random rand = new Random(1);
+
+        for (int i = 0; i < 50; i++) {
+            double w = rand.nextDouble()*10;
+            System.out.println("Connect 0 with " + (i+1) + ", weight " +  w);
+            graph.connect(0,i+1,w);
+        }
+
+        Assert.assertEquals(graph.edgeSize(), 50);
     }
-    /**
-     * Simple method for returning an array with all the node_data of the graph,
-     * Note: this should be using an Iterator<node_edge> to be fixed in Ex1
-     * @param g
-     * @return
-     */
-    private static int[] nodes(weighted_graph g) {
-        int size = g.nodeSize();
-        Collection<node_info> V = g.getV();
-        node_info[] nodes = new node_info[size];
-        V.toArray(nodes); // O(n) operation
-        int[] ans = new int[size];
-        for(int i=0;i<size;i++) {ans[i] = nodes[i].getKey();}
-        Arrays.sort(ans);
-        return ans;
+
+    @Test
+    @DisplayName("Connect nodes test")
+    public void ConnectivityTest(){
+        WGraph_DS graph = new WGraph_DS();
+        graph.addNode(1);
+        graph.addNode(2);
+        graph.addNode(3);
+        graph.addNode(4);
+        graph.connect(1,2,6);
+        graph.connect(1,4,5);
+        Assertions.assertEquals(graph.getEdge(1,2),6);
+        Assertions.assertEquals(graph.getEdge(2,1),6);
+        Assertions.assertEquals(graph.getEdge(1,4),5);
+        Assertions.assertEquals(graph.getEdge(4,1),5);
+        Assertions.assertTrue(graph.hasEdge(1, 2));
+        Assertions.assertTrue(graph.hasEdge(2, 1));
+        Assertions.assertTrue(graph.hasEdge(1, 4));
+        Assertions.assertTrue(graph.hasEdge(4, 1));
     }
 }
